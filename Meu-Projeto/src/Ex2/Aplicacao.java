1b5649ffcdd6b2025450dfe56530a6742f8a892d
Lucas-Bavaresco <bavaresco@infinitirs.com.br>
2021-02-07 18:00:26 -0300

    EXERCICIO 02 (COMPARADOR) - STATUS: ENVIADO PARA SER CORRIGIDO

---
+++ Meu-Projeto/src/Ex2/Aplicacao.java
@@ -0,0 +1,10 @@
+package Ex2;
+
+public class Aplicacao {
+
+    public static void main(String[] args) {
+        Comparador comparador = new Comparador();
+        boolean comparacao = comparador.menorQue(1,2);
+        System.out.println(comparacao);
+    }
+}
---
+++ Meu-Projeto/src/Ex2/Comparador.java
@@ -0,0 +1,9 @@
+package Ex2;
+
+public class Comparador {
+
+    public boolean menorQue (int num1, int num2) {
+        boolean menorQue = num1 < num2;
+        return menorQue;
+    }
+}
